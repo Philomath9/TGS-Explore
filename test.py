c57ba550414e000477ae39dec348048a9fa0399c
labdy-source <labdy@gregoryschool.org>
2026-02-20 11:35:29 -0700

    TGS-1 last commit rq

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 #Logan test idk
-numb = input("Enter a number: ")
-if(int(numb)%2==0):
-    print("Even")
-else:    print("Odd")
+testwhat = input("What module do you want to test? 1: Even or Odd? 2: Square, 3: Fibonacci")
+if(testwhat == "1"):
+    numb = input("Enter a number: ")
+    if(int(numb)%2==0):
+        print("Even")
+    else:    print("Odd")
+elif(testwhat == "2"):
+    numb = input("Enter a number: ")
+    print(int(numb)**2)
+elif(testwhat == "3"):
+    numb = input("Enter a number: ")
+    a, b = 0, 1
+    for i in range(int(numb)):
+        print(a)
+        a, b = b, a + b
+else:    print("Invalid input")
