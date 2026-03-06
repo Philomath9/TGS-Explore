ce6bfadd2c254f2393858c50259287adc752904b
labdy-source <labdy@gregoryschool.org>
2026-03-06 12:24:46 -0700

    #ant

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pymunk.pygame_util
 import math   
 import graphics
 
-screen, clock, draw_options, space, player_body = graphics.doGraphicsSetup()
+screen, clock, draw_options, space, player_body, player_image, animation, background = graphics.doGraphicsSetup()
 
 keys = {}
 running = True
@@ -17,14 +17,14 @@ while running:
         if event.type == pygame.KEYUP:
             keys[event.key] = False
 
-    # Controls
-    if keys.get(pygame.K_LEFT, False):
+    # Controls - Arrow keys or WASD
+    if keys.get(pygame.K_LEFT, False) or keys.get(pygame.K_a, False):
         player_body.apply_force_at_local_point((-800, 0), (0, 0))
-    if keys.get(pygame.K_RIGHT, False):
+    if keys.get(pygame.K_RIGHT, False) or keys.get(pygame.K_d, False):
         player_body.apply_force_at_local_point((800, 0), (0, 0))
-    if keys.get(pygame.K_SPACE, False) and abs(player_body.velocity.y) < 20:
+    if (keys.get(pygame.K_SPACE, False) or keys.get(pygame.K_w, False)) and abs(player_body.velocity.y) < 20:
         player_body.apply_impulse_at_local_point((0, -450), (0, -40))  # jump
 
-    graphics.doGraphicsStep(screen, clock, draw_options, space)
+    graphics.doGraphicsStep(screen, clock, draw_options, space, player_body, player_image, animation, background)
 
 pygame.quit()
